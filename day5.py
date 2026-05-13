8c1a8bd16e46291d2e7edb351fb3056a037ac050
navjotsingh06sn <clearnavjotsingh06sn@gmail.com>
2026-05-13 10:56:41 +0530

    today im learning

---
+++ day5.py
@@ -0,0 +1,39 @@
+# eceptional handling - prevent the program crashesh , easily error handle ,safe the code 
+#types of error
+# ValueError
+# ZeroDivisionError
+# SyntaxError
+# KeyError
+# InterruptedError
+# IndentationError
+# TypeError
+# FileNotFoundError
+# IndexError
+
+# try :
+#      number = int(input("enter the the your number : "))
+#      result= 40/number
+#      print("your result is : ",result)
+
+# except ZeroDivisionError:
+#      print("enter your no except 0")
+
+# except ValueError:
+#      print("enter the correct number :")
+
+# finally:
+#     print("programm executed....")
+
+
+# i=[2,3,4,5]
+# try: 
+#      print(i[5])
+# except IndexError:
+#      print("invalid index")        
+
+i=[2,3,4,5]
+# try: 
+print(i[5])
+# except Exception as ettt:
+# print("invalid index",ett)        
+     
